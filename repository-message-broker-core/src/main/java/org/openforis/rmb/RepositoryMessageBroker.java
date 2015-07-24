e95743adb441e968ad909f73de2ff73265c0de77
Daniel Wiell <daniel.wiell@fao.org>
2015-07-24 14:13:45 +0200

    Updated javadocs.

--- repository-message-broker-core/src/main/java/org/openforis/rmb/RepositoryMessageBroker.java
+++ repository-message-broker-core/src/main/java/org/openforis/rmb/RepositoryMessageBroker.java
@@ -33,13 +33,6 @@ public final class RepositoryMessageBroker implements MessageBroker {
         this.queueManager = new MessageQueueManager(config);
     }
 
-    /**
-     * Starts the message broker. This method must be invoked before publishing any message to a queue.
-     * This will fail if the broker already is started or has been stopped.
-     * <p>
-     * In addition to starting the broker, a shutdown hook will also be registered.
-     * </p>
-     */
     public void start() {
         if (!started.compareAndSet(false, true))
             throw new IllegalStateException("Message broker has already been started");
@@ -50,9 +43,6 @@ public final class RepositoryMessageBroker implements MessageBroker {
         monitors.onEvent(new MessageBrokerStartedEvent(this));
     }
 
-    /**
-     * Stops the message broker. This is a no-op if the broker never been started or already been stopped.
-     */
     public void stop() {
         if (started.get() && stopped.compareAndSet(false, true)) {
             queueManager.stop();
@@ -60,27 +50,12 @@ public final class RepositoryMessageBroker implements MessageBroker {
         }
     }
 
-    /**
-     * Provides a builder for creating {@link MessageQueue}s.
-     *
-     * @param queueId     the id of the queue to build. Must not be null and must be unique within the message broker.
-     * @param messageType the type of messages to be published to the queue. Must not be null.
-     * @param <M>         the type of messages to be published to the queue
-     * @return the queue builder
-     */
     public <M> MessageQueue.Builder<M> queueBuilder(String queueId, Class<M> messageType) {
         Is.hasText(queueId, "queueId must be specified");
         Is.notNull(messageType, "messageType must not be null");
         return new MessageQueue.Builder<M>(queueId, queueManager);
     }
 
-    /**
-     * Provides a builder for creating {@link MessageQueue}s.
-     *
-     * @param queueId the id of the queue to build. Must not be null and must be unique within the message broker.
-     * @param <M>     the type of messages to be published to the queue
-     * @return the queue builder
-     */
     public <M> MessageQueue.Builder<M> queueBuilder(String queueId) {
         Is.hasText(queueId, "queueId must be specified");
         return new MessageQueue.Builder<M>(queueId, queueManager);
--- repository-message-broker-core/src/main/java/org/openforis/rmb/spi/Clock.java
+++ repository-message-broker-core/src/main/java/org/openforis/rmb/spi/Clock.java
@@ -1,10 +1,27 @@
 package org.openforis.rmb.spi;
 
+/**
+ * A clock for measuring current time and sleeping.
+ */
 public interface Clock {
+    /**
+     * Gets the current time in milliseconds.
+     *
+     * @return current time in milliseconds
+     */
     long millis();
 
+    /**
+     * Causes the current thread to sleep for the provided amount of milliseconds.
+     *
+     * @param millis the number of milliseconds to sleep fore
+     * @throws InterruptedException when current thread is interrupted
+     */
     void sleep(long millis) throws InterruptedException;
 
+    /**
+     * A default {@link Clock} implementation.
+     */
     final class SystemClock implements Clock {
         public long millis() {
             return System.currentTimeMillis();
